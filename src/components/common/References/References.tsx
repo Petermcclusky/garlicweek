347fa21fd691b775f050ef8fdf0162a282f038ae
Dakota <ramensea0@gmail.com>
2023-09-19 11:34:51 -0700

    Updated the bottom to be green

--- src/components/common/References/References.tsx
+++ src/components/common/References/References.tsx
@@ -4,26 +4,50 @@ import { FacebookOutlined, InstagramOutlined, TwitterOutlined } from '@ant-desig
 
 export const References: React.FC = () => {
   return (
-    <S.ReferencesWrapper>
+    <S.ReferencesWrapper
+      style={{
+        color: "#008F3F",
+      }}
+    >
       <S.Text>
         {' '}
-        <a href="https://www.ontariogarlicweek.ca/" target="_blank" rel="noreferrer">
+        <a href="https://www.ontariogarlicweek.ca/" target="_blank" rel="noreferrer"
+           style={{
+             color: "#008F3F",
+           }}
+        >
           Ontario Garlic Week{' '}
         </a>
         is a project of {' '}
-        <a href="https://www.torontogarlicfestival.ca/" target="_blank" rel="noreferrer">
+        <a href="https://www.torontogarlicfestival.ca/" target="_blank" rel="noreferrer"
+          style={{
+            color: "#008F3F",
+          }}
+        >
           Toronto Garlic Festival
         </a>
-        (an Ontario not-for-profit)
+        &nbsp;(an Ontario not-for-profit)
       </S.Text>
       <S.Icons>
-        <a href="https://twitter.com/ONGarlicWeek" target="_blank" rel="noreferrer">
+        <a href="https://twitter.com/ONGarlicWeek" target="_blank" rel="noreferrer"
+           style={{
+             color: "#008F3F",
+           }}
+        >
           <TwitterOutlined />
         </a>
-        <a href="https://www.facebook.com/ontariogarlicweek" target="_blank" rel="noreferrer">
+        <a href="https://www.facebook.com/ontariogarlicweek" target="_blank" rel="noreferrer"
+           style={{
+             color: "#008F3F",
+           }}
+        >
           <FacebookOutlined />
         </a>
-        <a href="https://www.instagram.com/ontariogarlicweek/" target="_blank" rel="noreferrer">
+        <a href="https://www.instagram.com/ontariogarlicweek/" target="_blank" rel="noreferrer"
+           style={{
+             color: "#008F3F",
+           }}
+        >
           <InstagramOutlined />
         </a>
       </S.Icons>
